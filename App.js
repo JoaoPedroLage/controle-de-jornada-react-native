b751d91d6bac29e6a5ee92e5cf1ac8895e8c9d29
João Pedro Lage <joao.lage@grupofigueiredo.com.br>
2022-07-12 17:37:30 -0300

    feat: add duration time count and add new styles

--- App.js
+++ App.js
@@ -1,57 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   SectionList,
   Button,
-  SafeAreaView } from 'react-native';
+  SafeAreaView,
+  ScrollView } from 'react-native';
 import TableNTC from './Table'
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
 
   const [travelStatus, setTravelStatus] = useState('selecione o status');
-  const [DataTable, setDataTable] = useState([]);
-  const [TravelId, setTravelId] = useState(1)
+  const [storeTable, setStoreTable] = useState([]);
+  const [DataTable, setDataTable] = useState(storeTable === null ?  [] : storeTable);
+  const [TravelId, setTravelId] = useState(1);
+
+  useEffect( async () => {
+    await getData();
+  }, [storeTable]);
+
+  const storeData = async (value) => {
+    await AsyncStorage.setItem('@storage_Key', JSON.stringify(value));
+  }
+
+  const getData = async () => {
+      const value = await AsyncStorage.getItem('@storage_Key');
+      if(JSON.parse(value) !== null) {
+        return null;
+      } else setStoreTable(JSON.parse(value));
+  }
+
+  const calcDif = (dti, dtf) => {
+    const difTime = new Date(dti).getTime() - new Date(dtf).getTime();
+    const difSeg = ((Math.ceil(((difTime * -1) / 1000))));
+
+    if(Number(difSeg) >= 60) {
+      return (difSeg / 60).toFixed(2) + ' minutos';
+    } else {
+      return Math.ceil(difSeg) + ' segundos';
+    }
+  }
 
   function handleButton(status) {
-
-    const initialDate = new Date().toLocaleTimeString();
-    const endDate = new Date().toLocaleTimeString();
-
-    const calcDif = (dti, dtf) => {
-      console.log(dti, typeof dtf)
-      return '10 min'
-    }
+    const initialDate = new Date();
+    const endDate = new Date();
+    const statusName = ['Direção', 'Refeição', 'Descanso']
 
     if (DataTable.length >= 1) {
-      DataTable[(DataTable.length - 1)][2] = endDate
+      DataTable[(DataTable.length - 1)][2] = endDate.toLocaleString()
       const dti = DataTable[(DataTable.length - 1)][1]
       const dtf = DataTable[(DataTable.length - 1)][2]
       const dif = calcDif(dti, dtf)
       DataTable[(DataTable.length - 1)][3] = dif
     }
 
-    if(status === 1) {
-      setTravelStatus('1 - Direção')
-      DataTable.push([1, initialDate, '---', '---', TravelId]);
-      setDataTable([...DataTable])
-      setTravelId(TravelId + 1)
-    }
-    if(status === 2) {
-      setTravelStatus('2 - Refeição')
-      DataTable.push([2, initialDate, '---', '---', TravelId]);
-      setDataTable([...DataTable])
-      setTravelId(TravelId + 1)
-    }
-    if(status === 3) {
-      setTravelStatus('3 - Descanso')
-      DataTable.push([3, initialDate, '---', '---', TravelId]);
-      setDataTable([...DataTable])
-      setTravelId(TravelId + 1)
-    }
-
+    setTravelStatus(`${status} - ${statusName[status-1]}`)
+    DataTable.push([status, initialDate.toLocaleString(), '---', '---', TravelId]);
+    setDataTable([...DataTable]);
+    setTravelId(TravelId + 1);
+    storeData([...DataTable]);
   };
 
     return (
@@ -64,7 +73,7 @@ export default function App() {
             ]}
             renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
             renderSectionHeader={({section}) => <Text style={styles.title}>{section.title}</Text>}
-            keyExtractor={(item, index) => index}
+            keyExtractor={(_, index) => index}
           />
           <View style={styles.button}>
             <Text 
@@ -76,22 +85,26 @@ export default function App() {
               style={ styles.button }
               title="1 - Direção"
               accessibilityLabel=""
-              selected
+              disabled = { travelStatus === '1 - Direção' ? true : false }
             />
             <Button
               onPress={() => handleButton(2)}
               style={ styles.button }
               title="2 - Refeição"
               accessibilityLabel=""
+              disabled = { travelStatus === '2 - Refeição' ? true : false }
             />
             <Button
               onPress={() => handleButton(3)}
               style={ styles.button }
               title="3 - Descanso"
               accessibilityLabel=""
+              disabled = { travelStatus === '3 - Descanso' ? true : false }
             />
           </View>
-          <TableNTC DataTable={DataTable} /> 
+          {/* <ScrollView> */}
+            <TableNTC DataTable={DataTable} storeTable={storeTable} />
+          {/* </ScrollView> */}
         </View>
       </SafeAreaView>
     );
@@ -101,19 +114,20 @@ const styles = StyleSheet.create({
   container: {
     display: 'flex',
     backgroundColor: '#2A2A2A',
-    justifyContent: 'flex-start',
     alignItems: 'stretch',
     height: '100%'
   },
   text: {
     alignSelf: 'center',
     color: '#f0ffff',
-    marginBottom: 20,
+    fontSize: 30,
+    marginBottom: 10,
     fontSize: 20,
   },
   list: {
     display: 'flex',
     paddingTop: 50,
+    fontSize: 50,
     alignSelf: 'center',
   },
   title: {
@@ -123,11 +137,12 @@ const styles = StyleSheet.create({
     color: '#f0ffff',
   },
   item: {
-    fontSize: 20,
+    fontSize: 30,
     height: 60,
     color: '#f0ffff',
     alignSelf: 'center'
   }, button: {
+    fontSize: 30,
     margin: 10,
   }, HeadStyle: { 
     height: 50,
--- Table.js
+++ Table.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Table, Row, Rows } from 'react-native-table-component';
-
+// import Table from 'react-bootstrap/Button';
 
 class TableNTC extends React.Component { 
   
@@ -10,21 +10,31 @@ class TableNTC extends React.Component {
     const HeadTable = ['ID Tipo Jornada', 'Data Inicio', 'Data Fim', 'Duração', 'ID Jornada']
  
     return (
-      <Table>
-        <Row data={HeadTable} style={styles.HeadStyle} />
-        <Rows data={this.props.DataTable} style={styles.HeadStyle} />
+      <Table style={styles.Table}>
+        <Row data={HeadTable} style={styles.RowStyle} />
+        <Rows
+        // data={this.props.DataTable === null ? this.props.DataTable : this.props.storeTable}
+        data={this.props.DataTable}
+        style={styles.RowsStyle}
+        />
       </Table>
     )
- 
   }
-
 }
 
 const styles = StyleSheet.create({
-  HeadStyle: {
+  RowStyle: {
+    height: 80,
+    textAlign: "center",
+    alignItems: "center",
+    fontFamily: "Helvética",
+    backgroundColor: '#ff6347'
+  },
+  RowsStyle: {
     height: 50,
-    alignContent: "center",
-    backgroundColor: '#ffe0f0'
+    textAlign: "center",
+    alignItems: "center",
+    backgroundColor: '#e9967a'
   }
 });
 
