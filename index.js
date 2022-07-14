4b52cc9e489ce4c9ed9d340ef3ff841a0568d6ae
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-07-14 14:48:53 -0300

    feat: try a new way to make app work

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import { registerRootComponent } from 'expo';
+import AppProvider from './front-end/context/AppProvider';
+import App from './front-end/pages/Login';
 
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(
+  App,
+  () => (props) => (
+    <AppProvider>
+      <App {...props} />
+    </AppProvider>
+  ),
+  () => App
+);
