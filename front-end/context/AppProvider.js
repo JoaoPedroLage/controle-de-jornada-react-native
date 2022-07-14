f9a4e5dc6b1f2412d817e5209e0390ac8cb05d6a
JoaoPedroLage <joaopedro.lage@outlook.com>
2022-07-14 14:45:49 -0300

    feat: add a context  provider to app, to save date

---
+++ front-end/context/AppProvider.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import AppContext from './AppContext';
+
+function AppProvider({ children }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [dataDriverId, setDataDriverId] = useState('');
+
+  const context = {
+    email,
+    setEmail,
+    password,
+    setPassword,
+  };
+
+  return (
+    <AppContext.Provider value={ context }>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
